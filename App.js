b241d0f457760dd1de283d293d3a6599de2b1377
Yanely <yanely_rmz@yahoo.com>
2017-11-15 22:16:58 -0500

    changing to a blinking state

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { showText: true };
+
+		// Toggle the state every second
+		setInterval(() => {
+			this.setState(previousState => {
+				return { showText: !previousState.showText };
+			});
+		}, 1000);
+	}
+
 	render() {
+		let display = this.state.showText ? this.props.text : ' ';
 		return (
-			<Text>Hello {this.props.name}!</Text>
-		);
+			<Text>{display}</Text>
+		)
 	}
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
 	render() {
 		return (
 			<View style={{alignItems: 'center'}}>
-				<Greeting name='Fern'></Greeting>
-				<Greeting name='Jen'></Greeting>
-				<Greeting name='Greg'></Greeting>
+				<Blink text='I love to blink' />
+				<Blink text='Yes blinking is so great' />
+				<Blink text='Why did they ever take this out of HTML' />
+				<Blink text='Look at me look at me look at me' />
 			</View>
 		)
 	}
