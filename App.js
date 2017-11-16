fa04467cc56a209c14411f56c2f9d03c050a0f67
Yanely <yanely_rmz@yahoo.com>
2017-11-15 22:55:14 -0500

    creating a pizza translator (using the pizza emoji)

--- App.js
+++ App.js
@@ -1,46 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
 
-class FixedDimensions extends Component {
-	render() {
-		return (
-			<View style={{paddingTop: 30}}>
-				<Text style={{height: 50, backgroundColor: 'powderblue', color: 'red'}}>just red</Text>
-				<Text style={{width: 100, height: 100, backgroundColor: 'skyblue'}}>blue</Text>
-				<Text style={styles.smallGreen}>small green</Text>
-				<Text style={styles.bigGreen}>big green</Text>
-			</View>
-		);
+export default class PizzaTranslator extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {text: ''};
 	}
-}
 
-export default class FlexDimensions extends Component {
 	render() {
 		return(
-			<View style={{flex: 1}}>
-				<View style={{flex: 1, backgroundColor: 'yellow'}} />
-				<View style={{flex: 2, backgroundColor: 'brown'}} />
-				<View style={{flex: 3, backgroundColor: 'steelblue'}} />
+			<View style={{
+				padding: 10
+			}}>
+				<TextInput
+					style={{height: 40}}
+					placeholder='Type here to translate!'
+					onChangeText={(text) => this.setState({text})}
+				/>
+				<Text style={{padding: 10, fontSize: 42}}>
+					{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+				</Text>
 			</View>
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-	red: {
-		color: 'red',
-	},
-	bigGreen: {
-		color: 'green',
-		fontSize: 30,
-		fontWeight: 'bold'
-	},
-	smallGreen: {
-		color: 'green',
-		fontSize: 14,
-		fontWeight: '300',
-	},
-	blue: {
-		color: 'blue',
-	},
-});
