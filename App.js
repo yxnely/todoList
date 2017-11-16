fc4d24262fb04f266e97cbda6ae8672194093257
Yanely <yanely_rmz@yahoo.com>
2017-11-15 22:29:54 -0500

    playing around with fixed and flexed dimensions

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class LotsOfStyles extends Component {
+class FixedDimensions extends Component {
 	render() {
 		return (
 			<View style={{paddingTop: 30}}>
-				<Text style={styles.red}>just red</Text>
-				<Text style={styles.blue}>blue</Text>
+				<Text style={{height: 50, backgroundColor: 'powderblue', color: 'red'}}>just red</Text>
+				<Text style={{width: 100, height: 100, backgroundColor: 'skyblue'}}>blue</Text>
 				<Text style={styles.smallGreen}>small green</Text>
 				<Text style={styles.bigGreen}>big green</Text>
 			</View>
 		);
+	}
+}
+
+export default class FlexDimensions extends Component {
+	render() {
+		return(
+			<View style={{flex: 1}}>
+				<View style={{flex: 1, backgroundColor: 'yellow'}} />
+				<View style={{flex: 2, backgroundColor: 'brown'}} />
+				<View style={{flex: 3, backgroundColor: 'steelblue'}} />
+			</View>
+		)
 	}
 }
 
